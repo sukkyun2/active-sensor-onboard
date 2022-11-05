142fc82789da5aa2781e0046953d835f0b32041f
sukkyun <goobghd@gmail.com>
2022-11-05 22:08:13 +0900

    fix : lottie 의존성 충돌 해결
    - reactl-lottie => lottie-react

--- src/components/OnboardingContent.tsx
+++ src/components/OnboardingContent.tsx
@@ -1,6 +1,5 @@
 import { Content } from "./PageProps";
-// import animationData from "../assets/data/123760-cat-sneaking.json";
-import Lottie from "react-lottie";
+import Lottie from "lottie-react";
 
 const OnboardingContent = ({ title, subTitle, resource }: Content) => {
   return (
@@ -9,20 +8,12 @@ const OnboardingContent = ({ title, subTitle, resource }: Content) => {
       <small>{subTitle}</small>
       <div className="image-container">
         <Lottie
-          options={options(require("../assets/data/123760-cat-sneaking.json"))}
+          loop={true}
+          animationData={require("../assets/data/123760-cat-sneaking.json")}
         />
       </div>
     </div>
   );
 };
-
-const options = (animationData: any) => ({
-  loop: true,
-  autoplay: true,
-  animationData: animationData,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-});
 
 export default OnboardingContent;
